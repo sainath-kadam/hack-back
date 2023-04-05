6e50383b8e1ef2e7ad9ce6617c62af1aa9ad810b
sainath kadam <98304794+sainath-kadam@users.noreply.github.com>
2023-04-05 12:00:34 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -2,9 +2,16 @@ const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE
 
-mongoose.connect(DB,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-}).then(()=> console.log("DataBase Connected")).catch((errr)=>{
-    console.log(errr);
-})
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+        // make the process fail
+        process.exit(1);
+    }
